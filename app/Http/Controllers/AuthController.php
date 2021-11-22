332a1f48ae32fc2dde4ae75241a74902f378a463
Fahmi <fahminurfalah26@gmail.com>
2021-11-22 12:34:59 +0700

    create controller for auth and migration schema for create users table and configure model user

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register()
+    {
+    }
+}
