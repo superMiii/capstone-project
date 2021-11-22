77e40fe07aab1edb2d8280f94e16d727e274df3c
Fahmi <fahminurfalah26@gmail.com>
2021-11-22 11:50:55 +0700

    configure AuthServiceProvider in app.php and Routing in web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'api/v1'], function () use ($router) {
+    $router->post('/login', 'AuthController@login');
+    $router->post('/register', 'AuthController@register');
+});
