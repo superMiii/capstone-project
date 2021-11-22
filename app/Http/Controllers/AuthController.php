d1c6bf0656972db006e6341833bc4caa31a775fc
Fahmi <fahminurfalah26@gmail.com>
2021-11-22 13:25:12 +0700

    create validation for register in AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,7 +9,18 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
-    public function register()
+    public function register(Request $request)
     {
+        // validation
+        $validation = $this->validate($request, [
+            'name' => 'required|min:3|max:64|',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:6',
+        ]);
+
+        // restore data to each variable
+        $name = $validation['name'];
+        $email = $validation['email'];
+        $password = Hash::make($validation['name']);
     }
 }
