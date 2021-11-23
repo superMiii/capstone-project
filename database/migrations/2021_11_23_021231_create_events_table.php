40f1c2f7ca917cd611ba1f2b7ea49e2762b332e5
Fahmi <fahminurfalah26@gmail.com>
2021-11-23 09:33:12 +0700

    create migration file for schema events and categries table

---
+++ database/migrations/2021_11_23_021231_create_events_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('poster');
+            $table->string('time');
+            $table->string('place');
+            $table->string('date');
+            $table->string('regiter_link');
+            $table->string('ticket_price');
+            $table->integer('category_id');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
