b2e5d9117884fdac3da2e16985fc7f51aa2226f2
Fahmi <fahminurfalah26@gmail.com>
2021-11-23 14:23:21 +0700

    making read function for EventController

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Event;
 
 class EventController extends Controller
 {
-    public function getAllEvents()
+    public function showAll()
     {
+        $data = Event::paginate(10);
+
+        if ($data['data']) {
+            return response()->json([
+                'status' => 'success',
+                'message' => 'get data successfully',
+                'data' => $data
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'get data failed. Data not found',
+                'data' => []
+            ], 404);
+        }
     }
 
     //
--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,14 @@ $router->group(['prefix' => 'api/v1'], function () use ($router) {
     $router->post('/register', 'AuthController@register');
     $router->post('/login', 'AuthController@login');
 
+    // categories
     $router->get('/categories', 'CategoryController@showAll');
     $router->get('/categories/{id}', 'CategoryController@showById');
     $router->post('/categories', 'CategoryController@store');
     $router->put('/categories/{id}', 'CategoryController@update');
     $router->delete('/categories/{id}', 'CategoryController@destroy');
+
+    // events
+    $router->get('/events', 'EventController@showAll');
     $router->post('/logout', 'AuthController@logout');
 });
