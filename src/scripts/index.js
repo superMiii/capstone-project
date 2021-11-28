ebb2bc2710158e8f8a3e478bc594016cec77eb65
zayyid <mochamadzayyid@gmail.com>
2021-11-28 11:34:55 -0800

    tambah navbar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,9 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import component
+import './component/compnent_navbar';
 
 console.log('Hello Coders! :)');
