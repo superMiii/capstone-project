8f732c1b381b69bad7d3a30302fc663ca833b424
zayyid <mochamadzayyid@gmail.com>
2021-11-29 11:44:40 -0800

    menambahkan css pada sign in

--- src/scripts/views/pages/sign_in.js
+++ src/scripts/views/pages/sign_in.js
@@ -1,7 +1,19 @@
 const Sign_In = {
     async render() {
       return `
-        <h2>Sign In</h2>
+        <div class="container-sign-in">
+            <div class="sign-in">
+                <h1>Sign In</h1>
+                <form action="#">
+                <label for="email">Email:</label><br>
+                <input type="email" id="email" name="email"><br><br>
+                <label for="password">Password:</label><br>
+                <input type="password" id="password" name="password"><br><br>
+                <button class="btn-submit-sme" type="submit">Submit</button>
+                </form>
+                <p>Don’t have any account ? <a href="#/sign_up">Sign Up</a></p>
+            </div>
+        </div>
       `;
     },
    
