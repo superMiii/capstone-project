1800bb079ed52bb14ce010a7e73e1344b5334624
Fahmi <fahminurfalah26@gmail.com>
2021-11-30 10:38:10 +0700

    menambah source fetch data untuk events

--- src/scripts/data/events_source.js
+++ src/scripts/data/events_source.js
@@ -1,7 +1,66 @@
 import API_ENDPOINT from "../globals/api-endpoint";
 
 class EventsSource {
-    static async getAllEvents() {
-        const respose = await fetch(API_ENDPOINT.EVENTS)
+    static async AllEvents() {
+        const response = await fetch(API_ENDPOINT.EVENTS);
+        const responseJson = await response.json();
+        return responseJson.data;
+    }
+    
+    static async EventById(id) {
+        const response = await fetch(API_ENDPOINT.EVENT_DETAIL(id));
+        return response.json();
+    }
+    
+    static async EventByUserId(id, apiToken) {
+        const response = await fetch(API_ENDPOINT.EVENTS_USER(id, apiToken));
+        const responseJson = await response.json();
+        return responseJson.data;
+    }
+
+    static async LatestEvent(limit) {
+        const response = await fetch(API_ENDPOINT.EVENT_LATEST(limit));
+        const responseJson = await response.json();
+        return responseJson.data;
+    }
+
+    static async EventByCategory(id, apiToken) {
+        const response = await fetch(API_ENDPOINT.EVENTS_CATEGORY(id, apiToken));
+        const responseJson = await response.json();
+        return responseJson.data;
+    }
+
+    static async searchEvent(keyword) {
+        const response = await fetch(API_ENDPOINT.EVENT_SEARCH(keyword));
+        const responseJson = await response.json();
+        return responseJson.data;
+    }
+
+    static async addEvent(apiToken, data) {
+        const response = await fetch(API_ENDPOINT.ADD_EVENT(apiToken), {
+            method: 'POST',
+            body: data
+        });
+        const responseJson = await response.json();
+        return responseJson;
+    }
+
+    static async updateEvent(id, apiToken) {
+        const response = await fetch(API_ENDPOINT.UPDATE_EVENT(id, apiToken), {
+            method: 'POST',
+            body: data
+        });
+        const responseJson = await response.json();
+        return responseJson;
+    }
+
+    static async deleteEvent(id, apiToken) {
+        const response = await fetch(API_ENDPOINT.DELETE_EVENT(id, apiToken), {
+            method: 'DELETE'
+        });
+        const responseJson = await response.json();
+        return responseJson;
     }
 }
+
+export default EventsSource;
