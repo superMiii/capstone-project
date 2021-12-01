cdd8ba79798d68289c95af46ac4d4d4cc25a8fbc
zayyid <mochamadzayyid@gmail.com>
2021-12-01 13:04:26 -0800

    menambahkan modal pada detail

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -1,7 +1,9 @@
 const createDetailTemplate = () => `
     <div class="all-info-detail d-flex flex-row justify-content-evenly flex-wrap">
         <div class="image-detail">
-            <img src="https://asset-a.grid.id/crop/0x0:0x0/700x465/photo/haifoto/original/81932_foto-kebetulan-yang-keren-abis.jpg" style="width: 20rem;" alt="">
+            <button data-bs-toggle="modal" data-bs-target="#myModal">
+                <img src="https://asset-a.grid.id/crop/0x0:0x0/700x465/photo/haifoto/original/81932_foto-kebetulan-yang-keren-abis.jpg" alt="">
+            </button>
             <p>*click to enlarge image</p>
         </div>
         <div class="info-detail">
@@ -23,6 +25,21 @@ const createDetailTemplate = () => `
     <div class="description-detail">
         <h2>Description</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis molestias suscipit nemo! Et explicabo eveniet fugit doloribus, doloremque aliquam commodi voluptatibus ab, necessitatibus dolores quo. Quia, obcaecati. Labore, necessitatibus velit.</p>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="myModalLabel">Modal title</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <img src="https://asset-a.grid.id/crop/0x0:0x0/700x465/photo/haifoto/original/81932_foto-kebetulan-yang-keren-abis.jpg" class="card-img-top" alt="...">
+        </div>
+        </div>
+    </div>
     </div>
 `
 
