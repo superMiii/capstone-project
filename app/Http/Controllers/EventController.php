a1bdc4a01c076b0b2d5d15a746961173bfc1a366
Fahmi <fahminurfalah26@gmail.com>
2021-12-02 17:13:24 +0700

    update code

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -90,11 +90,11 @@ class EventController extends Controller
         }
     }
 
-    public function showByIdUser($id)
+    public function showByIdUser(Request $request)
     {
-        $user = User::find($id);
+        $user = User::where('api_token', $request->api_token)->get();
         if ($user) {
-            $data = Event::latest()->with(['user', 'category'])->where('user_id', $id)->paginate(10);
+            $data = Event::latest()->with(['user', 'category'])->where('user_id', $user[0]->id)->paginate(10);
             if ($data) {
                 return response()->json([
                     'status' => true,
