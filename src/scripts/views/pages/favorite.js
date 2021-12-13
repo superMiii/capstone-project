babd2f98be6eb359b4bb9f12808b7d4e5d35c96c
zayyid <mochamadzayyid@gmail.com>
2021-12-13 10:48:21 -0800

    menyelesaikan favorite

--- src/scripts/views/pages/favorite.js
+++ src/scripts/views/pages/favorite.js
@@ -1,6 +1,6 @@
 import logout from "../../utils/logout";
 import addClassActive from "../../utils/add-class-active";
-import EventsSource from "../../data/events-source";
+import FavoritesSource from "../../data/favorites-source";
 import { createCardEventTemplate } from "../templates/template-creator";
 
 const Favorite = {
@@ -24,12 +24,14 @@ const Favorite = {
       const allNavLink = document.querySelectorAll('.nav-item .nav-link');
       addClassActive(elementLinkNavFav, allNavLink);
 
-      const events = await EventsSource.allEvents(2);
-
-      // kodingan sementara @fahmi boleh diubah dengan API
+      // ambil data dari API
+      const userLocalStorage = JSON.parse(localStorage.getItem('user'));
+      const favorites = await FavoritesSource.allFavorites(userLocalStorage.api_token);
+      // manipulasi isi kontent
       const elementCard = document.querySelector(".inner-favorite");
-      events.data.forEach((event) => {
-        elementCard.innerHTML += createCardEventTemplate(event) ;
+      favorites.data.data.forEach((i) => {
+        console.log(i);
+        elementCard.innerHTML += createCardEventTemplate(i.event) ;
       });
 
       logout();
